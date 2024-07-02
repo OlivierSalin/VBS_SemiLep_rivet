0a998b0c54a830233ddc086871ab99b7d36651c4
OlivierSalin <olivier.salin@ens-paris-saclay.fr>
2024-07-02 18:12:21 +0200

    Simplified version of Rivet repo

--- table_test_rivet.sh
+++
@@ -1,8 +0,0 @@
-rivet-build RivetWpZ_llqq.so WpZ_llqq.cc -I/exp/atlas/salin/ATLAS/VBS_mc/vcpkg/installed/x64-linux/include/
-python run_rivet.py --evtMax 2000 --conf "user.osalin.MadGraph_WpZ_llqq_FM0_SM" --DOCUT "YES" --redoRivet "yes" --redoPlots "no"
-python run_rivet.py --evtMax 2000 --conf "user.osalin.MadGraph_WpZ_llqq_FS1_QUAD" --DOCUT "YES" --redoRivet "yes" --redoPlots "no"
-python run_rivet.py --evtMax 2000 --conf "user.osalin.MadGraph_WpZ_llqq_FT1_QUAD" --DOCUT "YES" --redoRivet "yes" --redoPlots "no"
-python run_rivet.py --evtMax 2000 --conf "user.osalin.MadGraph_WpZ_llqq_FT2_QUAD" --DOCUT "YES" --redoRivet "yes" --redoPlots "no"
-python run_rivet.py --evtMax 2000 --conf "user.osalin.MadGraph_WpZ_llqq_FM1_QUAD" --DOCUT "YES" --redoRivet "yes" --redoPlots "no"
-python run_rivet.py --evtMax 2000 --conf "user.osalin.MadGraph_WpZ_llqq_FM2_QUAD" --DOCUT "YES" --redoRivet "yes" --redoPlots "no"
-
--- test_rivet_run.sh
+++
@@ -1,8 +0,0 @@
-rivet-build RivetWpZ_llqq.so WpZ_llqq.cc -I/exp/atlas/salin/ATLAS/VBS_mc/vcpkg/installed/x64-linux/include/
-python run_rivet.py --evtMax 10000 --conf "user.osalin.MadGraph_WpZ_llqq_FM0_SM" --DOCUT "YES" --redoRivet "yes" --redoPlots "yes"
-python run_rivet.py --evtMax 10000 --conf "user.osalin.MadGraph_WpZ_llqq_FS1_QUAD" --DOCUT "YES" --redoRivet "yes" --redoPlots "yes"
-python run_rivet.py --evtMax 10000 --conf "user.osalin.MadGraph_WpZ_llqq_FT1_QUAD" --DOCUT "YES" --redoRivet "yes" --redoPlots "yes"
-
-python run_rivet.py --evtMax 10000 --conf "user.osalin.MadGraph_WpZ_llqq_FM0_SM" --DOCUT "NO" --redoRivet "yes" --redoPlots "yes"
-python run_rivet.py --evtMax 10000 --conf "user.osalin.MadGraph_WpZ_llqq_FS1_QUAD" --DOCUT "NO" --redoRivet "yes" --redoPlots "yes"
-python run_rivet.py --evtMax 10000 --conf "user.osalin.MadGraph_WpZ_llqq_FT1_QUAD" --DOCUT "NO" --redoRivet "yes" --redoPlots "yes"
